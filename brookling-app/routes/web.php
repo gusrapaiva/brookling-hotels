1e421b72fd67e799bb3ac28d5f7b3927f0954d50
gusrapaiva <gustavorapaiva@gmail.com>
2024-04-12 22:39:10 -0300

    login com breeze

--- brookling-app/routes/web.php
+++ brookling-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\FuncionarioController;
@@ -8,37 +9,64 @@ use App\Http\Controllers\ReservaController;
 
 
 // ----------------------------- \\
-Route::get("/",[ClienteController::class, 'showHome']);
 
-// Grupo de Cliente
-Route::post('/cadastro-cliente', [ClienteController::class,'cadCliente'])->name('envia-db-clie');
-Route::get('/cadastro-cliente',[ClienteController::class,'showformularioCadastro']);
-Route::get('/gerenciar-cliente/{id}',[ClienteController::class,'formAlterarCliente'])->name('mostrar-cliente');
 
-Route::get('/gerenciar-cliente', [ClienteController::class, 'gerenciarCliente'])->name('gerenciar-cliente');
-Route::put('/alterar-cliente/{id}', [ClienteController::class, 'alterClie'])->name('alterar-cliente');
-Route::delete('/apagar-cliente/{id}', [ClienteController::class, 'destroy'])->name('apagar-cliente');
 
-// Grupo de Funcionario
-Route::post('/cadastro-funcionario', [FuncionarioController::class,'cadFunc'])->name('envia-db-func');
-Route::get('/cadastro-funcionario',[FuncionarioController::class,'showformularioCadastroFun']);
-Route::get('/gerenciar-funcionario/{id}',[FuncionarioController::class,'formAlterarFunc'])->name('editar-funcionario');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::put('/alterar-funcionario/{id}', [FuncionarioController::class, 'alterarFunc'])->name('alterar-funcionario');
-Route::get('/gerenciar-funcionario', [FuncionarioController::class, 'gerenciarFuncionario'])->name('gerenciar-funcionario');
-Route::delete('/apagar-funcionario/{id}', [FuncionarioController::class, 'destroy'])->name('apagar-funcionario');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Grupo de Quartos
-Route::get('/cadastro-quarto',[QuartoController::class,'showformularioCadastroQuarto']);
-Route::post('/cadastro-quarto', [QuartoController::class,'cadQuarto'])->name('envia-db-quarto');
-Route::get('/gerenciar-quarto/{id}',[QuartoController::class,'formAlterarQuarto'])->name("editar-quarto");
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/gerenciar-quarto', [QuartoController::class, 'gerenciarQuarto'])->name('gerenciar-quarto');
-Route::delete('/apagar-quarto/{id}', [QuartoController::class, 'destroy'])->name('apagar-quarto');
-Route::put('/alterar-quarto/{id}', [QuartoController::class, 'alterarQuarto'])->name('alterar-quarto');
+Route::middleware('auth')->group(function () {
+    Route::get("/",[ClienteController::class, 'showHome']);
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Grupo de Cliente
+    Route::post('/cadastro-cliente', [ClienteController::class,'cadCliente'])->name('envia-db-clie');
+    Route::get('/cadastro-cliente',[ClienteController::class,'showformularioCadastro']);
+    Route::get('/gerenciar-cliente/{id}',[ClienteController::class,'formAlterarCliente'])->name('mostrar-cliente');
 
-// Grupo da Reserva
-Route::get('/cadastro-reserva',[ReservaController::class,'showformularioCadastroReserva']);
-Route::post('/cadastro-reserva', [ReservaController::class,'cadReserva'])->name('envia-db-reserva');
-Route::get('/gerenciar-reserva',[ReservaController::class,'gerenciarReserva']);
+    Route::get('/gerenciar-cliente', [ClienteController::class, 'gerenciarCliente'])->name('gerenciar-cliente');
+    Route::put('/alterar-cliente/{id}', [ClienteController::class, 'alterClie'])->name('alterar-cliente');
+    Route::delete('/apagar-cliente/{id}', [ClienteController::class, 'destroy'])->name('apagar-cliente');
 
+    // Grupo de Funcionario
+    Route::post('/cadastro-funcionario', [FuncionarioController::class,'cadFunc'])->name('envia-db-func');
+    Route::get('/cadastro-funcionario',[FuncionarioController::class,'showformularioCadastroFun']);
+    Route::get('/gerenciar-funcionario/{id}',[FuncionarioController::class,'formAlterarFunc'])->name('editar-funcionario');
+
+    Route::put('/alterar-funcionario/{id}', [FuncionarioController::class, 'alterarFunc'])->name('alterar-funcionario');
+    Route::get('/gerenciar-funcionario', [FuncionarioController::class, 'gerenciarFuncionario'])->name('gerenciar-funcionario');
+    Route::delete('/apagar-funcionario/{id}', [FuncionarioController::class, 'destroy'])->name('apagar-funcionario');
+
+    // Grupo de Quartos
+    Route::get('/cadastro-quarto',[QuartoController::class,'showformularioCadastroQuarto']);
+    Route::post('/cadastro-quarto', [QuartoController::class,'cadQuarto'])->name('envia-db-quarto');
+    Route::get('/gerenciar-quarto/{id}',[QuartoController::class,'formAlterarQuarto'])->name("editar-quarto");
+
+    Route::get('/gerenciar-quarto', [QuartoController::class, 'gerenciarQuarto'])->name('gerenciar-quarto');
+    Route::delete('/apagar-quarto/{id}', [QuartoController::class, 'destroy'])->name('apagar-quarto');
+    Route::put('/alterar-quarto/{id}', [QuartoController::class, 'alterarQuarto'])->name('alterar-quarto');
+
+    // Grupo da Reserva
+    Route::get('/cadastro-reserva',[ReservaController::class,'showformularioCadastroReserva']);
+    Route::post('/cadastro-reserva', [ReservaController::class,'cadReserva'])->name('envia-db-reserva');
+    Route::get('/gerenciar-reserva',[ReservaController::class,'gerenciarReserva']);
+});
+
+require __DIR__.'/auth.php';
